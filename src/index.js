f476dcd929ef49e8e9018e8c6bbd40d918248b68
Drew Henderson <drewhendersonemail@gmail.com>
2022-02-07 14:44:35 -0800

    add dotenv support
    Co-Authored By: Kevin Williams <kevin@k9w.org>
    Co-authored-by: Ryan Bass <theryanbass@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+
+
 $(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    //const city = $('#location').val();
-    $('#location').val("");
+  $('#submit').click(function() {
+    let search = $('#search').val();
+    if (search.trim() === "") {
+      search = "cats";
+    }
+    $('#search').val("");
 
     let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/search?q=cats&api_key=Ilcu6Bgq2HiCCSQsiyZFMrMkm4TYthCV`;
-    console.log(url);
-
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=cats&limit=50&offset=0&rating=g&lang=en`;
+    //const url = `https://api.giphy.com/v1/gifs/trending?api_key=Ilcu6Bgq2HiCCSQsiyZFMrMkm4TYthCV`;
     request.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
         const response = JSON.parse(this.responseText);
